74dc4afc8ce783405b1232629f415869337ffa3e
Isep Lutpi Nur <53908272+upi20@users.noreply.github.com>
2023-03-26 16:26:51 +0700

    home add team

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -81,10 +81,213 @@
         <!-- Feature Section End Here -->
     @endif
 
+    <!-- Team section start here -->
+    <div class="team-section padding-tb shape-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="header-title">
+                        <h5>Pengurus</h5>
+                        <h2>Para pengajar di Rumah Tahfidzh Quran Ar-Rahman</h2>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/01.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Hamad Bin Jasim</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/02.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Sayyida Al-Hijaazi</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/03.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Ashraf Al-Maktum</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/04.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Ayesha Binte Alif</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/05.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Hamad Bin Jasim</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/06.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Sayyida Al-Hijaazi</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/07.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Ashraf Al-Maktum</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-sm-6 col-12">
+                            <div class="card mb-4 text-center border-none team-item-1 pattern-2">
+                                <div class="lab-inner">
+                                    <div class="lab-thumb">
+                                        <img data-src="{{ asset('assets/templates/frontend/images/team/08.jpg') }}"
+                                            class="card-img-top lazy" alt="product">
+                                    </div>
+                                    <div class="lab-content">
+                                        <a href="#">
+                                            <h6 class="card-title mb-0">Ayesha Binte Alif</h6>
+                                        </a>
+                                        <p class="card-text mb-3">Hafiz Quran Scholor</p>
+                                        <div class="social-share">
+                                            <a href="#" class="m-1 twitter"><i class="icofont-twitter"></i></a>
+                                            <a href="#" class="m-1 behance"><i class="icofont-behance"></i></a>
+                                            <a href="#" class="m-1 instagram"><i class="icofont-instagram"></i></a>
+                                            <a href="#" class="m-1 vimeo"><i class="icofont-vimeo"></i></a>
+                                            <a href="#" class="m-1 linkedin"><i class="icofont-linkedin"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Team section end here -->
+
     @php $k = "$p.galeri"; @endphp
     @if (settings()->get("$k.visible"))
         <!-- Galeri Section start here -->
-        <section class="event-section padding-tb padding-b shape-4">
+        <section class="event-section padding-b">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
@@ -231,9 +434,9 @@
                                         @if ($v->type == 2)
                                             <a href="{{ $v->link }}" style="width: 100%">
                                         @endif
-                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                            data-bs-target="#collapse{{ $k }}" aria-expanded="true"
-                                            aria-controls="collapse{{ $k }}">
+                                        <button class="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#collapse{{ $k }}"
+                                            aria-expanded="true" aria-controls="collapse{{ $k }}">
                                             {{ $v->nama }}
                                         </button>
                                         @if ($v->type == 2)
