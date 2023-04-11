9a94e2ac8e05305d7874259f95e4d23807487037
Isep Lutpi Nur <53908272+upi20@users.noreply.github.com>
2023-04-11 23:13:47 +0700

    update halaman login

--- resources/js/views/pages/admin/auth/login.js
+++ resources/js/views/pages/admin/auth/login.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    $('#year').text((new Date()).getFullYear());
     $('#Loginform').submit(function (e) {
         e.preventDefault();
         var formData = new FormData(this);
@@ -65,125 +64,16 @@ $(document).ready(function () {
         }
     });
 
-    particlesJS('particles-js', {
-        "particles": {
-            "number": {
-                "value": 80,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "window",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "grab"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "repulse"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true
-    });
-});
-
-function setBtnLoading(element, text, status = true) {
-    const el = $(element);
-    if (status) {
-        el.attr("disabled", "");
-        el.html(`<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"> </span> ${text}`);
-    } else {
-        el.removeAttr("disabled");
-        el.html(text);
+    function setBtnLoading(element, text, status = true) {
+        const el = $(element);
+        if (status) {
+            el.attr("disabled", "");
+            el.html(
+                `<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"> </span> ${text}`
+            );
+        } else {
+            el.removeAttr("disabled");
+            el.html(text);
+        }
     }
-}
+});
